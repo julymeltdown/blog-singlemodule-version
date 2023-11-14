63e96c20c24580cf69f5defacd9981c2c6b4faf0
julymeltdown <quintuplets2000@gmail.com>
2023-11-14 19:33:45 +0900

    [BE CHORE] deploy scripts added

---
+++ scripts/start.sh
@@ -0,0 +1,18 @@
+PROJECT_ROOT="/home/ubuntu/scripts"
+JAR_FILE="$PROJECT_ROOT/blog -0.0.1-SNAPSHOT.jar"
+
+APP_LOG="$PROJECT_ROOT/application.log"
+ERROR_LOG="$PROJECT_ROOT/error.log"
+DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
+
+TIME_NOW=$(date +%c)
+
+echo "$TIME_NOW : copy $JAR_FILE" >> $DEPLOY_LOG
+cp $PROJECT_ROOT/build/libs/*.jar $PROJECT_ROOT
+
+echo "$TIME_NOW : run $JAR_FILE" >> $DEPLOY_LOG
+chmod 111 JAR_FILE
+nohup java -jar $JAR_FILE > $APP_LOG 2> $ERROR_LOG &
+
+CURRENT_PID=$(pgrep -f $JAR_FILE)
+echo "$TIME_NOW : process id is $CURRENT_PID" >> $DEPLOY_LOG
