f8563d1eec9f3ffc77f92ce9a42d4d57d9dbe248
julymeltdown <quintuplets2000@gmail.com>
2023-11-14 19:38:24 +0900

    [BE FIX] deploy scripts error fixed

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,5 +1,5 @@
 PROJECT_ROOT="/home/ubuntu/scripts"
-JAR_FILE="$PROJECT_ROOT/blog -0.0.1-SNAPSHOT.jar"
+JAR_FILE="$PROJECT_ROOT/build/libs/blog -0.0.1-SNAPSHOT.jar"
 
 APP_LOG="$PROJECT_ROOT/application.log"
 ERROR_LOG="$PROJECT_ROOT/error.log"
